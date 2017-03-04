46b2825112784987a3679d49a1bee6436eda4c48
Dustin <Dustin>
2017-03-04 15:54:23 -0800

    dumb ai implementation

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express')
 var router  = express.Router()
 var taunts = ["Z", "A", "L", "G", "O"]
-var moves = ['up', 'left', 'down', 'right']
+var dumbmoves = ['up', 'left', 'down', 'right']
+var moves = ['up', 'down', 'left', 'right']
 var state = {
   width: 0,
   height: 0,
@@ -11,10 +12,7 @@ var state = {
     bad: [],
     heads: [],
   }
-}
-
-function nearestFood(){
-
+  nextMove: 'up',
 }
 
 // Handle POST request to '/start'
@@ -57,15 +55,57 @@ router.post('/move', function (req, res) {
   }
 
   // find nearby spots and make sure they aren't deadly
-
-
-  // Response data
-  var data = {
-    move: moves[req.body.turn % moves.length], // one of: ['up','down','left','right']
-    taunt: taunts[req.body.turn % taunts.length], // optional, but encouraged!
+  var possMoves = []
+  var testSpots = [
+    [state.coords[0], state.coords[1] - 1],
+    [state.coords[0], state.coords[1] + 1],
+    [state.coords[0] - 1, state.coords[1]],
+    [state.coords[0] + 1, state.coords[1]],
+  ]
+  for(var i=0; i<testSpots.length; i++){
+    if(state.spots.bad.indexOf(testSpots[i]) === -1){
+      //spot isn't lethal
+      possMoves.push(testSpots[i])
+    }
   }
-  console.log(state.coords[0][0])
 
+  //if one of the possMoves spots has food, yolo, treat yo self
+  for(var i=0; i<possMoves.length; i++){
+    if(state.spots.good.indexOf(possMoves[i]) != -1){
+      state.nextMove = moves[testSpots.indexOf(possMoves[i])]
+      var data = {
+        move: state.nextMove,
+        taunt: 'YUM',
+      }
+      return res.json(data)
+    }
+  }
+
+  //if we got here, there is no adjacent food. try to find if there's food aligned with us i guess, idk
+  var alignedFoods = []
+  for(var j=0; j<state.spots.good.length; j++){
+    if(state.spots.good[j][0] == state.coords[0] || state.spots.good[j][1] == state.coords[1]){
+      alignedFoods.push(state.spots.good[j])
+    }
+  }
+  //todo: check the aligned foods and find the closest
+  // for(var i=0; i<alignedFoods.length; i++){
+  //
+  // }
+
+  // so there's no easily findable food. pick a valid move at random if there are any.
+  if(possMoves.length > 0){
+    state.nextMove = moves[testSpots.indexOf(possMoves[Math.floor(Math.random() * possMoves.length)])]
+  }else{
+    //no non-lethal moves? rip, ascend to your death
+    state.nextMove = 'up'
+  }
+
+  var data = {
+    move: state.nextMove,
+    //move: moves[req.body.turn % dumbmoves.length],
+    taunt: taunts[req.body.turn % taunts.length],
+  }
   return res.json(data)
 })
 
